e7fac93a0e9bbc72d82bf292e697bc9d54cf044f
Alejandro <josealejandrogomezparedes@gmail.com>
2024-04-27 13:51:46 -0500

    iniciando con las templates

--- app1/camera/analysis.py
+++ app1/camera/analysis.py
@@ -1,40 +1,40 @@
-import tensorflow as tf
+# import tensorflow as tf
 import cv2
 import numpy as np
-import keras
+# import keras
 from app1 import constanst 
-from keras.applications.resnet50 import preprocess_input, decode_predictions, ResNet50
+# from keras.applications.resnet50 import preprocess_input, decode_predictions, ResNet50
 
-def GetTempPhoto():
-    img = cv2.imread(constanst.TempImgPath)
-    return img
+# def GetTempPhoto():
+#     img = cv2.imread(constanst.TempImgPath)
+#     return img
 
-# class Photo:
-#     def __init__(self, img):
-#         self.data = img
+# # class Photo:
+# #     def __init__(self, img):
+# #         self.data = img
 
-#     def PrintPhoto(self):
-#         print(self.data)
+# #     def PrintPhoto(self):
+# #         print(self.data)
 
-#     def getItems(self):
-#         model = ResNet50(weights='imagenet')
-#         img= keras.utils.load_img(constanst.TempImgPath, target_size=(224, 224))
-#         array = keras.utils.array_to_img(img)
-#         array = np.expand_dims(array, axis=0)
-#         array = preprocess_input(array)
-#         prediction = model.predict(array)
-#         print('Predicted:', decode_predictions(prediction, top=3)[0])
+# #     def getItems(self):
+# #         model = ResNet50(weights='imagenet')
+# #         img= keras.utils.load_img(constanst.TempImgPath, target_size=(224, 224))
+# #         array = keras.utils.array_to_img(img)
+# #         array = np.expand_dims(array, axis=0)
+# #         array = preprocess_input(array)
+# #         prediction = model.predict(array)
+# #         print('Predicted:', decode_predictions(prediction, top=3)[0])
 
-#     def getKeyWords(self):
-#         pass
+# #     def getKeyWords(self):
+# #         pass
 
 
-def getItems():
-    #model = ResNet50(weights='imagenet')
-    model = tf.keras.applications.InceptionV3(weights='imagenet')
-    img= keras.utils.load_img(constanst.TestImgPath, target_size=(299, 299))
-    array = keras.utils.array_to_img(img)
-    array = np.expand_dims(array, axis=0)
-    array = preprocess_input(array)
-    prediction = model.predict(array)
-    print('Predicted:', decode_predictions(prediction, top=4)[0])
+# def getItems():
+#     #model = ResNet50(weights='imagenet')
+#     model = tf.keras.applications.InceptionV3(weights='imagenet')
+#     img= keras.utils.load_img(constanst.TestImgPath, target_size=(299, 299))
+#     array = keras.utils.array_to_img(img)
+#     array = np.expand_dims(array, axis=0)
+#     array = preprocess_input(array)
+#     prediction = model.predict(array)
+#     print('Predicted:', decode_predictions(prediction, top=4)[0])

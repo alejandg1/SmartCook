365ffa1ee0d5aab069ba6e8787d5683609f33563
Alejandro <josealejandrogomezparedes@gmail.com>
2024-05-13 15:51:10 -0500

    add: singUp, login, session validations

--- security/forms/singin.py
+++ security/forms/singin.py
@@ -1,12 +1,17 @@
-from django import forms
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from security.models import User
 
 
-class SinginForm(ModelForm):
+class SingupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-        widgets = {
-            'password': forms.PasswordInput(),
+        fields = ['first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2']
+        labels = {
+            'first_name': 'Nombre',
+            'last_name': 'Apellidos',
+            'username': 'Nombre de usuario',
+            'email': 'Correo electrónico',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar contraseña'
         }
--- security/views.py
+++ security/views.py
@@ -1,21 +1,15 @@
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, CreateView
 from django.shortcuts import redirect
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from security.models import User
-from security.forms import edit, singin
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
-
-# Create your views here.
+from security.forms import edit, singin
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
-    model = User
     template_name = 'profile.html'
-    context_object_name = 'profile'
-    success_url = reverse_lazy('smartcook:index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -24,22 +18,28 @@ class ProfileView(LoginRequiredMixin, TemplateView):
 
 
 class EditView(LoginRequiredMixin, FormView):
-    model = User
     template_name = 'edit.html'
-    context_object_name = 'edit_user'
-    success_url = reverse_lazy('security:profile')
     form_class = edit.EditForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['edit'] = User.objects.get(pk=self.request.user.pk)
+        context['user'] = User.objects.get(pk=self.request.user.pk)
+        context['back_url'] = reverse_lazy('smartcook:index')
+        context['title'] = 'editar usuario'
+        context['button'] = 'Guardar'
         return context
+
+    def form_valid(self, form):
+        user = User.objects.get(pk=self.request.user.pk)
+        user.username = form.cleaned_data['username']
+        user.email = form.cleaned_data['email']
+        user.set_password(form.cleaned_data['password'])
+        user.save()
+        return redirect('security:profile')
 
 
 class LoginView(LoginView):
     template_name = 'login.html'
-    # context_object_name = 'login'
-    # form_class = login.LoginForm
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -50,28 +50,48 @@ class LoginView(LoginView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'inicio de sesión'
         context['button'] = 'Login'
+        context['back_url'] = reverse_lazy('smartcook:index')
         return context
 
 
-@login_required
-def LogoutView(request):
-    logout(request)
-    return redirect('smartcook:index')
+class LogoutView(LoginRequiredMixin, TemplateView):
+    template_name = 'logout.html'
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('smartcook:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
-class SinginView(LoginView):
-    template_name = 'singin.html'
-    # context_object_name = 'singin'
-    # success_url = reverse_lazy('smartcook:index')
-    # form_class = singin.SinginForm
+class SingUpView(CreateView):
+    template_name = 'singup.html'
+    form_class = singin.SingupForm
+    success_url = reverse_lazy('smartcook:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'registro'
+        context['button'] = 'Registrar'
+        context['back_url'] = reverse_lazy('smartcook:index')
+        return context
+
+    def form_valid(self, form):
+        self.object = form.save()
+        form.instance.set_password(form.cleaned_data['password1'])
+        form.instance.save()
+        user = authenticate(username=form.cleaned_data['username'],
+                            password=form.cleaned_data['password1'])
+        if user is not None:
+            login(self.request, user)
+            return redirect(self.success_url)
+        else:
+            form.add_error(None, 'Error al iniciar sesión')
+        return super().form_valid(form)
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('smartcook:index')
         return super().dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Registro'
-        context['button'] = 'Singin'
-        return context

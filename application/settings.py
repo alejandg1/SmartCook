5d405501ce7de173863a723c5bde84557efcb30d
Alejandro <josealejandrogomezparedes@gmail.com>
2024-05-24 18:04:17 -0500

    add: edit user profile, button component

--- application/settings.py
+++ application/settings.py
@@ -90,8 +90,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = '/static/'  # url de archivos estaticos
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'  # url de imagenes
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
--- security/forms/login.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from security.models import User
-
-
-class LoginForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
